6ae71b2f43a4397119c6575e162eb57b1765d659
Irshad Ali <m.irshad.ali21@gmail.com>
2022-08-02 18:03:52 +0500

    adding storage link route

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,18 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/clear-cache', function () {
+    $exitCode = Artisan::call('config:clear');
+    $exitCode = Artisan::call('cache:clear');
+    $exitCode = Artisan::call('config:cache');
+    $exitCode = Artisan::call('optimize:clear');
+    return 'DONE'; //Return anything
+});
+
+Route::get('/clear-cache', function () {
+    $exitCode = Artisan::call('storage:link');
+    return 'storage linked'; //Return anything
+});
 
 Route::get('/', function () {
     return redirect()->route('home');
