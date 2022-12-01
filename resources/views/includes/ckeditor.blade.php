d525f6c4213eecdabe060630dc561f8208fadc0f
Irshad Ali <m.irshad.ali21@gmail.com>
2022-12-01 18:16:54 +0500

    ammendments

--- resources/views/includes/ckeditor.blade.php
+++ resources/views/includes/ckeditor.blade.php
@@ -3,9 +3,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Prata&family=Roboto:wght@100&display=swap" rel="stylesheet">
     <style>
-        p{
+        p {
             all: revert;
         }
+
         #container {
             width: 1000px;
             margin: 20px auto;
@@ -29,6 +30,8 @@
             border-radius: 50%;
         }
     </style>
+    {{-- <script src="{{ asset('vendor/ckeditor53/build/ckeditor.js') }}"></script>
+    <script src="{{ asset('vendor/ckeditor53/sample/styles.css') }}"></script> --}}
 @endpush
 
 @push('scripts')
@@ -36,120 +39,305 @@
     {{-- <script src="https://cdn.ckeditor.com/ckeditor5/35.3.2/super-build/ckeditor.js"></script> --}}
     <script src="{{ asset('vendor/ckeditor421/ckeditor.js') }}"></script>
     <script>
-   
-   ckapplyeditor('body1');
-   ckapplyeditor('firms');
-   ckapplyeditor('benefits');
-   ckapplyeditor('benefits_in_number');
-   ckapplyeditor('tax_breack');
-   ckapplyeditor('source');
+        var wordCountConf1 = {
+            showParagraphs: false,
+            showWordCount: true,
+            showCharCount: true,
+            countSpacesAsChars: false,
+            countHTML: false,
+            maxWordCount: -1,
+            maxCharCount: 1500
+        }
 
-        function ckapplyeditor(params) {
-             CKEDITOR.replace(document.getElementById(params))
-            } 
-            //,{
-            //     toolbar: {
-            //         items: [
-            //             'heading', '|',
-            //             'bold', 'italic', 'underline', 'subscript', 'superscript', '|',
-            //             'bulletedList', 'numberedList', '|',
-            //             'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', '|',
-            //             'alignment', 
-            //         ],
-            //         // shouldNotGroupWhenFull: true,
-            //         colorButton_colors: '008000,454545,FFF', 
-            //         colorButton_enableMore: true, 
 
-            //     },
-            //     // Changing the language of the interface requires loading the language file using the <script> tag.
-            //     // language: 'es',
-            //     list: {
-            //         properties: {
-            //             styles: true,
-            //             startIndex: true,
-            //             reversed: true
-            //         }
-            //     },
-            //     // https://ckeditor.com/docs/ckeditor5/latest/features/headings.html#configuration
-            //     heading: {
-            //         options: [
-            //             {
-            //                 model: 'heading5',
-            //                 view: 'h5',
-            //                 title: 'Heading 5',
-            //                 class: 'ck-heading_heading5'
-            //             },
-            //             {
-            //                 model: 'paragraph',
-            //                 title: 'Paragraph',
-            //                 class: 'ck-heading_paragraph'
-            //             },
-            //             {
-            //                 model: 'heading1',
-            //                 view: 'h1',
-            //                 title: 'Heading 1',
-            //                 class: 'ck-heading_heading1'
-            //             },
-            //             {
-            //                 model: 'heading2',
-            //                 view: 'h2',
-            //                 title: 'Heading 2',
-            //                 class: 'ck-heading_heading2'
-            //             },
-            //             {
-            //                 model: 'heading3',
-            //                 view: 'h3',
-            //                 title: 'Heading 3',
-            //                 class: 'ck-heading_heading3'
-            //             },
-            //             {
-            //                 model: 'heading4',
-            //                 view: 'h4',
-            //                 title: 'Heading 4',
-            //                 class: 'ck-heading_heading4'
-            //             },
-                        
-            //             {
-            //                 model: 'heading6',
-            //                 view: 'h6',
-            //                 title: 'Heading 6',
-            //                 class: 'ck-heading_heading6'
-            //             }
-            //         ]
-            //     },
-            //     // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-family-feature
-            //     fontFamily: {
-            //         options: [
-            //             'Prata, serif',
-            //             'Roboto, sans-serif',
-            //             'Arial, Helvetica, sans-serif',
-            //             'Courier New, Courier, monospace',
-            //             'Georgia, serif',
-            //         ],
-            //         supportAllValues: true
-            //     },
-            //     // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-size-feature
-            //     fontSize: {
-            //         options: [10, 12, 14, 'default', 18, 20, 22],
-            //         supportAllValues: true
-            //     },
+        var wordCountConf2 = {
+            showParagraphs: false,
+            showWordCount: true,
+            showCharCount: true,
+            countSpacesAsChars: false,
+            countHTML: false,
+            maxWordCount: -1,
+            maxCharCount: 5000
+        }
+
+        // CKEDITOR.replace( document.querySelector( '#body' ) )
+        // CKEDITOR.replace(document.getElementById('bod1'), {wordcount: wordCountConf1})
+        CKEDITOR.replace(document.getElementById('body'), {
+            wordcount: wordCountConf1,
+            height: '470'
+        })
+        CKEDITOR.replace(document.getElementById('body1'), {
+            wordcount: wordCountConf1,
+            height: '470'
+        })
+        CKEDITOR.replace(document.getElementById('firms'), {
+            wordcount: wordCountConf1,
+            height: '155'
+        })
+        CKEDITOR.replace(document.getElementById('benefits'), {
+            wordcount: wordCountConf1,
+            height: '155'
+        })
+        CKEDITOR.replace(document.getElementById('benefits_in_number'), {
+            wordcount: wordCountConf1,
+            height: '155'
+        })
+        CKEDITOR.replace(document.getElementById('tax_breack'), {
+            wordcount: wordCountConf1,
+            height: '155'
+        })
+        CKEDITOR.replace(document.getElementById('source'), {
+            wordcount: wordCountConf1,
+            height: '150'
+        })
+
+        function changeclass(clas, add, remove) {
+            $(clas).addClass(add);
+            $(clas).removeClass(remove);
+        }
+
+        $('#chipuo_checkbox').on('click', function() {
+            if (document.getElementById('chipuo_checkbox').checked) {
+                changeclass('#lavelina_body', "col-sm-7", "col-sm-12")
+                $('#contatiner1').show();
+                $('#firms_div').show();
+            } else {
+                if (document.getElementById('percosa_checkbox').checked) {
+                    changeclass('#lavelina_body', "col-sm-7", "col-sm-12")
+                    $('#contatiner1').show();
+                    $('#firms_div').hide();
+                } else {
+                    changeclass('#lavelina_body', "col-sm-12", "col-sm-7")
+                    $('#contatiner1').hide();
+                    $('#firms_div').hide();
+                }
+            }
+        })
+        $('#percosa_checkbox').on('click', function() {
+            if (document.getElementById('percosa_checkbox').checked) {
+                changeclass('#lavelina_body', "col-sm-7", "col-sm-12")
+                $('#contatiner1').show();
+                $('#benefits_div').show();
+            } else {
+                if (document.getElementById('chipuo_checkbox').checked) {
+                    changeclass('#lavelina_body', "col-sm-7", "col-sm-12")
+                    $('#contatiner1').show();
+                    $('#benefits_div').hide();
+                } else {
+                    changeclass('#lavelina_body', "col-sm-12", "col-sm-7")
+                    $('#contatiner1').hide();
+                    $('#benefits_div').hide();
+                }
+            }
+        })
+
+
+
+        //body 2 
+
+        $(document).on('click', '.body2div', function() {
+            if (document.getElementById('quanto_checkbox').checked || document.getElementById('quali_checkbox')
+                .checked || document.getElementById('body2_checkbox').checked || document.getElementById(
+                    'fonti_checkbox').checked) {
+                $('#body2_row').show();
+            } else {
+                $('#body2_row').hide();
+            }
+        })
+
+
+        $('#body2_checkbox').on('click', function() {
+            if (document.getElementById('body2_checkbox').checked) {
+                $('#body2_div').show();
+                if (document.getElementById('quanto_checkbox').checked || document.getElementById('quali_checkbox')
+                    .checked) {
+                    $('#body2right_div').show();
+                    changeclass('#body2_div', "col-sm-7", "col-sm-12")
+                    changeclass('#body2right_div', "col-sm-5", "col-sm-12")
+                } else {
+                    $('#body2right_div').hide();
+                    changeclass('#body2right_div', "col-sm-12", "col-sm-5")
+                    changeclass('#body2_div', "col-sm-12", "col-sm-7")
+                }
+            } else {
+                $('#body2_div').hide();
+                if (document.getElementById('quanto_checkbox').checked || document.getElementById('quali_checkbox')
+                    .checked) {
+                    $('#body2right_div').show();
+                    changeclass('#body2right_div', "col-sm-12", "col-sm-5")
+                } 
+            }
+        })
+
+        $('#quanto_checkbox').on('click', function() {
+            if (document.getElementById('quanto_checkbox').checked) {
+                $('#body2right_div').show();
+                $('#benefits_in_number_div').show();
+                if (document.getElementById('body2_checkbox').checked)
+                {
+                    changeclass('#body2_div', "col-sm-7", "col-sm-12")
+                    changeclass('#body2right_div', "col-sm-5", "col-sm-12")
+                } else {
+                    changeclass('#body2right_div', "col-sm-12", "col-sm-5")
+                }
+            } else {
+                $('#benefits_in_number_div').hide();
+                if (!document.getElementById('quali_checkbox').checked) {
+                    changeclass('#body2_div', "col-sm-12", "col-sm-7")
+                    $('#body2right_div').hide();
+                }
+            }
+        })
+        $('#quali_checkbox').on('click', function() {
+            if (document.getElementById('quali_checkbox').checked) {
+                $('#body2right_div').show();
+                $('#tax_breack_div').show();
+                if (document.getElementById('body2_checkbox').checked)
+                {
+                    changeclass('#body2_div', "col-sm-7", "col-sm-12")
+                    changeclass('#body2right_div', "col-sm-5", "col-sm-12")
+                } else if (!document.getElementById('body2_checkbox').checked) {
+                    changeclass('#body2right_div', "col-sm-12", "col-sm-5")
+                }
+            } else {
+                $('#tax_breack_div').hide();
+                if (!document.getElementById('quanto_checkbox').checked) {
+                    changeclass('#body2_div', "col-sm-12", "col-sm-7")
+                    $('#body2right_div').hide();
+                }
+            }
+        })
+
+        $('#fonti_checkbox').on('click', function() {
+            if (document.getElementById('fonti_checkbox').checked) {
+                $('#source_div').show();
                 
-            //     // https://ckeditor.com/docs/ckeditor5/latest/features/link.html#custom-link-attributes-decorators
-            //     link: {
-            //         decorators: {
-            //             addTargetToExternalLinks: true,
-            //             defaultProtocol: 'https://',
-            //             toggleDownloadable: {
-            //                 mode: 'manual',
-            //                 label: 'Downloadable',
-            //                 attributes: {
-            //                     download: 'file'
-            //                 }
-            //             }
-            //         }
-            //     },  
-                       
-            // } 
+            } else {
+                $('#source_div').hide();
+            }
+        })
+
+
+        // ckapplyeditor('body1');
+        // ckapplyeditor('firms');
+        // ckapplyeditor('benefits');
+        // ckapplyeditor('benefits_in_number');
+        // ckapplyeditor('tax_breack');
+        // ckapplyeditor('source');
+
+        // function ckapplyeditor(params) {
+
+        //     CKEDITOR.replace
+        //     .create(document.querySelector('#source'))
+        //         .catch(error => {
+        //             console.error(error);
+        //         });
+        // }
+        //  CKEDITOR.replace(document.getElementById(params))
+        // } 
+        //,{
+        //     toolbar: {
+        //         items: [
+        //             'heading', '|',
+        //             'bold', 'italic', 'underline', 'subscript', 'superscript', '|',
+        //             'bulletedList', 'numberedList', '|',
+        //             'fontSize', 'fontFamily', 'fontColor', 'fontBackgroundColor', '|',
+        //             'alignment', 
+        //         ],
+        //         // shouldNotGroupWhenFull: true,
+        //         colorButton_colors: '008000,454545,FFF', 
+        //         colorButton_enableMore: true, 
+
+        //     },
+        //     // Changing the language of the interface requires loading the language file using the <script> tag.
+        //     // language: 'es',
+        //     list: {
+        //         properties: {
+        //             styles: true,
+        //             startIndex: true,
+        //             reversed: true
+        //         }
+        //     },
+        //     // https://ckeditor.com/docs/ckeditor5/latest/features/headings.html#configuration
+        //     heading: {
+        //         options: [
+        //             {
+        //                 model: 'heading5',
+        //                 view: 'h5',
+        //                 title: 'Heading 5',
+        //                 class: 'ck-heading_heading5'
+        //             },
+        //             {
+        //                 model: 'paragraph',
+        //                 title: 'Paragraph',
+        //                 class: 'ck-heading_paragraph'
+        //             },
+        //             {
+        //                 model: 'heading1',
+        //                 view: 'h1',
+        //                 title: 'Heading 1',
+        //                 class: 'ck-heading_heading1'
+        //             },
+        //             {
+        //                 model: 'heading2',
+        //                 view: 'h2',
+        //                 title: 'Heading 2',
+        //                 class: 'ck-heading_heading2'
+        //             },
+        //             {
+        //                 model: 'heading3',
+        //                 view: 'h3',
+        //                 title: 'Heading 3',
+        //                 class: 'ck-heading_heading3'
+        //             },
+        //             {
+        //                 model: 'heading4',
+        //                 view: 'h4',
+        //                 title: 'Heading 4',
+        //                 class: 'ck-heading_heading4'
+        //             },
+
+        //             {
+        //                 model: 'heading6',
+        //                 view: 'h6',
+        //                 title: 'Heading 6',
+        //                 class: 'ck-heading_heading6'
+        //             }
+        //         ]
+        //     },
+        //     // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-family-feature
+        //     fontFamily: {
+        //         options: [
+        //             'Prata, serif',
+        //             'Roboto, sans-serif',
+        //             'Arial, Helvetica, sans-serif',
+        //             'Courier New, Courier, monospace',
+        //             'Georgia, serif',
+        //         ],
+        //         supportAllValues: true
+        //     },
+        //     // https://ckeditor.com/docs/ckeditor5/latest/features/font.html#configuring-the-font-size-feature
+        //     fontSize: {
+        //         options: [10, 12, 14, 'default', 18, 20, 22],
+        //         supportAllValues: true
+        //     },
+
+        //     // https://ckeditor.com/docs/ckeditor5/latest/features/link.html#custom-link-attributes-decorators
+        //     link: {
+        //         decorators: {
+        //             addTargetToExternalLinks: true,
+        //             defaultProtocol: 'https://',
+        //             toggleDownloadable: {
+        //                 mode: 'manual',
+        //                 label: 'Downloadable',
+        //                 attributes: {
+        //                     download: 'file'
+        //                 }
+        //             }
+        //         }
+        //     },  
+
+        // } 
         //     )   )         
         // }
 
@@ -162,10 +350,10 @@
                 <!-- body Field ` + window.count + ` -->
                 <div class="form-group col-sm-6" id="lavelina_body">
                     {!! Form::label('body', 'Testo articolo `+window.count+`:') !!}<span style="color:red">(less then 2350)</span>
-                    <textarea name="body[]" class="form-control" id="body`+window.count+`"></textarea>
+                    <textarea name="body[]" class="form-control" id="body` + window.count + `"></textarea>
                 </div>
                 `)
-            ckapplyeditor('body'+window.count);
+            ckapplyeditor('body' + window.count);
 
         });
     </script>
--- resources/views/lavelina/fields.blade.php
+++ resources/views/lavelina/fields.blade.php
@@ -1,82 +1,128 @@
-<!-- Name Field -->
-<div class="form-group col-sm-4">
-    {!! Form::label('name', 'LA VELINA Name:') !!}
-    {!! Form::text('name', null, ['class' => 'form-control']) !!}
-</div>
+<div class="row">
 
-<!-- advisor Field -->
-<div class="form-group col-sm-4">
-    {!! Form::label('advisor', 'Advisor:') !!}
-    <select name="advisor" id="advisor" class="form-control select2">
-        @foreach ($advisor as $adv)
-            <option value="{{ $adv->id }}"
-                @isset($lavelina)
+    <!-- Name Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('name', 'LA VELINA Name:') !!}
+        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <!-- advisor Field -->
+    <div class="form-group col-sm-4">
+        {!! Form::label('advisor', 'Advisor:') !!}
+        <select name="advisor" id="advisor" class="form-control select2">
+            @foreach ($advisor as $adv)
+                <option value="{{ $adv->id }}"
+                    @isset($lavelina)
                 @if ($lavelina->advisor_id == $adv->id)
                     Selected
                 @endif
             @endisset>
-                {{ $adv->name }}</option>
-        @endforeach
-    </select>
-</div>
-
-<div class="form-group col-sm-4">
-    {!! Form::label('title', 'Tttolo articolo:') !!}
-    {!! Form::text('title', null, ['class' => 'form-control']) !!}
-</div>
-<div class="col-md-12">
-    <hr>
-</div>
-
-<!-- title Field -->
-<div class="col-sm-6">
-    Add another Testo articolo
-    <button class="button_plus" id="add_body" type="button">+</button>
-</div>
-
-<!-- color Field -->
-<div class="col-sm-6">
-    Color
-    <input name="color" class="form-control" type="color" value="#6a1109" id="example-color-input">
-</div>
-
-<div id="body_div" class=" col-sm-12 row">
-
-    <!-- body Field -->
-    <div class="form-group col-sm-6" id="lavelina_body">
-        
-        {!! Form::label('body', 'Testo articolo: ') !!}<span style="color:red">(less then 1500)</span>
-        <textarea name="body[]" id="body1" class="form-control "></textarea>
+                    {{ $adv->name }}</option>
+            @endforeach
+        </select>
     </div>
 
+    <div class="form-group col-sm-4">
+        {!! Form::label('title', 'Tttolo articolo:') !!}
+        {!! Form::text('title', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="col-md-12">
+        <hr>
+    </div>
+
+    <div class="form-group col-sm-2">
+        <label>Body</label>
+        <input type="checkbox" checked disabled>
+    </div>
+    <div class="form-group col-sm-2">
+        <label for="chipuo_checkbox">Chi Può</label>
+        <input type="checkbox" name="chipuo_checkbox" id="chipuo_checkbox">
+    </div>
+    <div class="form-group col-sm-2">
+        <label for="percosa_checkbox">Per Cosa</label>
+        <input type="checkbox" name="percosa_checkbox" id="percosa_checkbox">
+    </div>
+
+ <!-- color Field -->
+ <div class="col-sm-6">
+    {{-- <label for="example-color-input">color</label> --}}
+    <input name="color" class="form-control" type="color" value="#6a1109" id="example-color-input">
+</div>
+    
+    {{-- <!-- title Field -->
+    <div class="col-sm-6">
+        Add another Testo articolo
+        <button class="button_plus" id="add_body" type="button">+</button>
+    </div> --}}
+
+   
 </div>
 
-<!-- firms Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('firms', 'Chi Può:') !!}
-    {!! Form::textarea('firms', null, ['class' => 'form-control']) !!}
+<!-- body Field -->
+<div id="body_div" class="row">
+    <div class="form-group col-sm-12" id="lavelina_body" style="">
+        {!! Form::label('body', 'Testo articolo: ') !!}<span style="color:red">(less then 1500)</span>
+        <textarea name="body[]" id="body" class="form-control" rows="23"></textarea>
+    </div>
+
+    <div class="form-group col-sm-5" id="contatiner1" style="display: none">
+        <div id="firms_div" style="display: none">
+            <!-- firms Field -->
+            {!! Form::label('firms', 'Chi Può:') !!}
+            {!! Form::textarea('firms', null, ['class' => 'form-control']) !!}
+        </div>
+        <div id="benefits_div" style="display: none">
+            <!-- benefits Field -->
+            {!! Form::label('benefits', 'Per Cosa:') !!}
+            {!! Form::textarea('benefits', null, ['class' => 'form-control ']) !!}
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="form-group col-sm-4">
+        <label for="body2_checkbox">Body Page 2</label>
+        <input type="checkbox" class="body2div" name="body2_checkbox" id="body2_checkbox">
+    </div>
+    <div class="form-group col-sm-4">
+        <label for="quanto_checkbox">Quanto</label>
+        <input type="checkbox" class="body2div" name="quanto_checkbox" id="quanto_checkbox">
+    </div>
+    <div class="form-group col-sm-4">
+        <label for="quali_checkbox">Quali agevolazioni:</label>
+        <input type="checkbox" class="body2div" name="quali_checkbox" id="quali_checkbox">
+    </div>
+</div>
+<div id="body2_row" class="row" style="display: none">
+    <div class="form-group col-sm-12" style="display: none" id="body2_div">
+        {!! Form::label('body', 'Testo articolo: ') !!}<span style="color:red">(less then 1500)</span>
+        <textarea name="body[]" id="body1" class="form-control" rows="23"></textarea>
+    </div>
+
+    <!-- Benefits Quantity Field -->
+    <div class="form-group col-sm-5" style="display: none" id="body2right_div">
+        <div id="benefits_in_number_div" style="display: none">
+            {!! Form::label('benefits_in_number', 'Quanto:') !!}
+            {!! Form::textarea('benefits_in_number', null, ['class' => 'form-control ']) !!}
+        </div>
+        <div id="tax_breack_div" style="display: none">
+            <!-- tax_breack Field -->
+            {!! Form::label('tax_breack', 'Quali agevolazioni:') !!}
+            {!! Form::textarea('tax_breack', null, ['class' => 'form-control ']) !!}
+        </div>
+    </div>
 </div>
 
-<!-- benefits Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('benefits', 'Per Cosa:') !!}
-    {!! Form::textarea('benefits', null, ['class' => 'form-control ']) !!}
+<div class="row">
+    <div class="form-group col-sm-2">
+        <label for="fonti_checkbox">Fonti</label>
+        <input type="checkbox" class="body2div" name="fonti_checkbox" id="fonti_checkbox">
+    </div>
 </div>
-
-<!-- Benefits Quantity Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('benefits_in_number', 'Quanto:') !!}
-    {!! Form::textarea('benefits_in_number', null, ['class' => 'form-control ']) !!}
-</div>
-
-<!-- tax_breack Field -->
-<div class="form-group col-sm-6" style="height:350px">
-    {!! Form::label('tax_breack', 'Quali agevolazioni:') !!}
-    {!! Form::textarea('tax_breack', null, ['class' => 'form-control ']) !!}
-</div>
-
-<!-- source Field -->
-<div class="form-group col-sm-6" style="height:350px">
-    {!! Form::label('source', 'Fonti:') !!}
-    {!! Form::textarea('source', null, ['class' => 'form-control ']) !!}
+<div class="row" id="source_div" style="display: none">
+    <!-- source Field -->
+    <div class="form-group col-sm-12">
+        {!! Form::label('source', 'Fonti:') !!}
+        {!! Form::textarea('source', null, ['class' => 'form-control ']) !!}
+    </div>
 </div>
